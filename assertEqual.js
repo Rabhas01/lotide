1937ec27ef41f636cf8493ba4f5b8f4a2a2bc13f
Rabhas01 <rabhas.vashisht@gmail.com>
2022-03-07 22:50:40 +0000

    Implement assertEqual

---
+++ assertEqual.js
@@ -0,0 +1,13 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("✅✅✅Assertion Passed:" +  actual + " === " + expected);
+  } else {
+    console.log("👻👻👻 Assertion Failed:" + actual + " !== " + expected);
+  }
+};
+
+
+assertEqual("abc", "abc");
+assertEqual("abc", "abcs");
+assertEqual("123", "123");
+assertEqual("123", "321");
