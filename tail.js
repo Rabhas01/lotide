282402929405e72e3870c5826f63782d0bdfcbf7
Rabhas01 <rabhas.vashisht@gmail.com>
2022-03-08 01:57:18 +0000

    Implement tail

--- tail.js
+++ tail.js
@@ -15,19 +15,19 @@ const tail = function(arry) {
 
 
  
- let newArry = ["Hello", "Lighthouse", "Labs"]  
+let newArry = ["Hello", "Lighthouse", "Labs"];
 const result = tail([newArry]);
-assertEqual(result.length, 2); 
-assertEqual(result[0], "Lighthouse"); 
-assertEqual(result[1], "Labs"); 
-assertEqual(newArry.length, 3); 
+assertEqual(result.length, 2);
+assertEqual(result[0], "Lighthouse");
+assertEqual(result[1], "Labs");
+assertEqual(newArry.length, 3);
 
 
-let newArry2 = ["Hello"]  
+let newArry2 = ["Hello"];
 const result2 = tail([newArry2]);
-assertEqual(result2.length, 0); 
+assertEqual(result2.length, 0);
 
 
-let newArry3 = []  
+let newArry3 = [];
 const result3 = tail([newArry3]);
-assertEqual(result3.length, 0); 
+assertEqual(result3.length, 0);
