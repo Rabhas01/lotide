4683f088788da3aa61a9edd950d8b289d243c937
Rabhas01 <rabhas.vashisht@gmail.com>
2022-03-10 00:16:53 +0000

    count Lettters

--- counLetters.js
+++ counLetters.js
@@ -17,6 +17,8 @@ const assertEqual = function(actual, expected) {
   }
   return obj;
 };
-assertEqual(countLetters("lighthouse in the house"),{ l: 1, i: 2, g: 1, h: 4, t: 2, o: 2, u: 2, s: 2, e: 3, n: 1 }) 
+
+let obj = countLetters("lighthouse in");
+assertEqual(obj["i"], 2);
 
 
