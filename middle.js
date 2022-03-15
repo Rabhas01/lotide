d6bbe2bc6eda8f56f18afb56076456065b6dec66
Rabhas <rabhas.vashisht@gmail.com>
2022-03-15 12:19:34 +0000

    Test written using mocha and chai

--- middle.js
+++ middle.js
@@ -1,36 +1,3 @@
-const eqArrays = function(arry1, arry2){
-    if(arry1.length === arry2.length){
-        for (let i = 0; i < arry1.length; i++){
-            if(arry1[i] !== arry2[i] ){
-                return false
-            }
-        }
-        return true
-    }
-    else {
-        return false
-    }
-}
-
-const assertArraysEqual = function(arry1, arry2){
-    let result = eqArrays(arry1, arry2)
-    if( result === true ){
-        console.log(`Assertion Passed: ${arry1}  ===  ${arry2}`)
-    }else{
-        console.log(`Assertion failed: ${arry1}  !==  ${arry2}`)
-    }
-
-}
-
-const assertEqual = function(actual, expected) {
-    if (actual === expected) {
-      console.log(`✅✅✅Assertion Passed:" ${actual}  ===  ${expected}`);
-    } else {
-      console.log(`👻👻👻 Assertion Failed:  ${actual}   !==   ${expected}`);
-    }
-  };
-
-
 const middle = function(array){
     let middleElement = [];
     if (array.length % 2 === 0){
@@ -45,5 +12,5 @@ const middle = function(array){
    
 }
 
-assertArraysEqual(middle([7,8,9,1]), [8,9])
-assertArraysEqual(middle(["a", "b", "c", "d", "e"]), ["c"])
+
+module.exports = middle;
